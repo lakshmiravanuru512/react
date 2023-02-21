94ec1a7e0e9307b3b7ef6c066f19e7df90925ca5
lakshmiravanuru512 <lakshmirevanuru512@gmail.com>
2023-02-21 21:14:49 +0530

    functions examples

---
+++ function.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const function1 =()=>{
+    return (
+        <div>
+            <h1>Header</h1>
+            <h2>Status</h2>
+        </div>
+    )
+}
+
+const function2 =()=>(
+    <div>
+        <h1>Header</h1>
+        <h2>Footer</h2>
+    </div>
+)
