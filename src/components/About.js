342be46b3288185970ff3efd102d2b2519e0ea27
lakshmiravanuru512 <lakshmirevanuru512@gmail.com>
2023-03-10 11:55:17 +0530

    showing errors using useRouterError

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ const About=()=>{
     return (
         <div>
         <h1>This is AboutUs Page</h1>
-        <p>This is a React class chapter-07</p>
+        <p>This is a React class chapter-07 Finding Path</p>
         </div>
     )
 }
