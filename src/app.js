fc4638beda1365fd9aae6289318a5b8076a5fb05
lakshmiravanuru512 <lakshmirevanuru512@gmail.com>
2023-03-10 11:48:11 +0530

    routing path to about and error page

--- src/app.js
+++ src/app.js
@@ -6,7 +6,9 @@ import Header from "./components/Header";
 // import {Title} from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
-
+import About from "./components/About";
+import Error from "./components/Error";
+import { createBrowserRouter ,RouterProvider } from "react-router-dom";
 
 
 
@@ -21,7 +23,19 @@ import Footer from "./components/Footer";
         </>
     )
 }
+const appRouter=createBrowserRouter([
+  {
+    path : "/",
+  element:<AppLayout/>,
+  errorElement:<Error/>
+},{
+  path:"/about",
+  element:<About/>
+}
+
+
+])
 
 const root= ReactDOM.createRoot (document.getElementById("root"));
 
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+const About=()=>{
+    return (
+        <div>
+        <h1>This is AboutUs Page</h1>
+        <p>This is a React class chapter-07</p>
+        </div>
+    )
+}
+
+export default About
