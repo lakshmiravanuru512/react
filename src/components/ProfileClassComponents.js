f69216d92e39302f6868263ccae5e85171bd3d1e
lakshmiravanuru512 <lakshmirevanuru512@gmail.com>
2023-03-14 14:46:51 +0530

    class components

--- src/components/ProfileClassComponents.js
+++ src/components/ProfileClassComponents.js
@@ -16,6 +16,8 @@ class Profile extends React.Component{
             <h2>name:{this.props.name}</h2>
             <h3>Count:{this.state.Count}</h3>
             <button onClick={()=>{
+                //we do not mutate state directly
+                // never do this.state=something
                 this.setState({
                     Count:1,
                     count2:2,
