5294f04faee161b8d3f1191047f3c4dcde0c23bc
lakshmiravanuru512 <lakshmirevanuru512@gmail.com>
2023-03-20 17:54:33 +0530

    show hide example

--- src/components/Instamart.js
+++ src/components/Instamart.js
@@ -1,9 +1,18 @@
+import { useState } from "react";
+
 const Section=({title,description})=>{
+    const[isVisible,setIsVisible]=useState(false);
 return(
     <div className="border border-black p-2 m-2">
        <h3 className=" font-bold text-xl ">{title}</h3>
-       <p>{description}</p>
+       {isVisible? (<button onClick={()=>setIsVisible(false)} className="cursor-pointer underline">Hide</button>) : (<button onClick={()=>setIsVisible(true)} className="cursor-pointer underline">Show</button>)
+       }
+      
+      {isVisible && <p>{description}</p>}
+       
+       
     </div>
+   
 );
 };
 
