66eacf5a3f28105e49c1d7ae962d0f1513e05700
lakshmiravanuru512 <lakshmirevanuru512@gmail.com>
2023-03-21 16:43:21 +0530

    accordian component

--- src/components/Instamart.js
+++ src/components/Instamart.js
@@ -17,48 +17,34 @@ return(
 
 
 const Instamart=()=>{
-    const[sectionConfig, setSectionConfig]=useState({
-        showAbout:false,
-        showTeam:false,
-        showCarrer:false
-    })
+   const [visibleSection, setVisibleSection]=useState("about")
     return (
         <div>
             <h1 className="text-3xl p-2 m-2">Instamart</h1>
             <Section  
             title={"About Instamart"}
             description={"The base structure is a large multi-chambered cube with chamfered corners forming an unequal eight-sided structure that is approximately 55 metres (180 ft) on each of the four long sides. Each side of the iwan is framed with a huge pishtaq or vaulted archway with two similarly shaped arched balconies stacked on either side. This motif of stacked pishtaqs is replicated on the chamfered corner areas, making the design completely symmetrical on all sides of the building. Four minarets frame the tomb, one at each corner of the plinth facing the chamfered corners. The main chamber houses the false sarcophagi of Mumtaz Mahal and Shah Jahan; the actual graves are at a lower level.[20]"}
-            isVisible={sectionConfig.showAbout}
+            isVisible={visibleSection==="about"}
             setIsVisible={()=>
-            setSectionConfig({
-                showAbout:true,
-                showTeam:false,
-                showCarrer:false
-            })}
+                setVisibleSection("about")
+            }
             
             />
             <Section  
             title={"Team Instamart"}
             description={"The base structure is a large multi-chambered cube with chamfered corners forming an unequal eight-sided structure that is approximately 55 metres (180 ft) on each of the four long sides. Each side of the iwan is framed with a huge pishtaq or vaulted archway with two similarly shaped arched balconies stacked on either side. This motif of stacked pishtaqs is replicated on the chamfered corner areas, making the design completely symmetrical on all sides of the building. Four minarets frame the tomb, one at each corner of the plinth facing the chamfered corners. The main chamber houses the false sarcophagi of Mumtaz Mahal and Shah Jahan; the actual graves are at a lower level.[20]"}
-            isVisible={sectionConfig.showTeam}
+            isVisible={visibleSection==="team"}
             setIsVisible={()=>
-            setSectionConfig({
-                showAbout:false,
-                showTeam:true,
-                showCarrer:false
-            })}
+           setVisibleSection("team")
+            }
             
             />
              <Section  
             title={"Carrer of Instamart"}
             description={"The base structure is a large multi-chambered cube with chamfered corners forming an unequal eight-sided structure that is approximately 55 metres (180 ft) on each of the four long sides. Each side of the iwan is framed with a huge pishtaq or vaulted archway with two similarly shaped arched balconies stacked on either side. This motif of stacked pishtaqs is replicated on the chamfered corner areas, making the design completely symmetrical on all sides of the building. Four minarets frame the tomb, one at each corner of the plinth facing the chamfered corners. The main chamber houses the false sarcophagi of Mumtaz Mahal and Shah Jahan; the actual graves are at a lower level.[20]"}
-            isVisible={sectionConfig.showCarrer}
+            isVisible={visibleSection==="carrer"}
             setIsVisible={()=>
-            setSectionConfig({
-                showAbout:false,
-                showTeam:false,
-                showCarrer:true
-            })}
+           setVisibleSection("carrer")}
             
             />
         </div>
